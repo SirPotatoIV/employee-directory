bdec60c2361a77467dd10ad7c98252879e44f8bb
SirPotatoIV <otoolej33@gmail.com>
2020-01-22 18:08:15 -0600

    Successfully recieved a random employee from the api. Added axios module.

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,16 @@
+const axios = require("axios")
+
 function main(){
-    console.log("test")
+    // console.log("test")
+    async function getRandomEmployees(){
+        try{
+            const response = await axios.get("https://randomuser.me/api/")
+            console.log(response.data)
+        }
+        catch(err){
+            console.log("error getting employees from api: ", err)
+        }
+    }
+    getRandomEmployees()
 }
 main()
